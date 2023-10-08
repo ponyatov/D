194943393ac843587adc9cc05e4c545191438e95
Dmitry Ponyatov <dponyatov@gmail.com>
2023-10-08 12:01:41 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- source/app.d
+++ source/app.d
@@ -1,19 +1,85 @@
 // https://www.youtube.com/live/8ESoL8d8GrE?si=Vsmb1cIHiuyl7i7g&t=1475
 
 import std.stdio : writeln;
-
+import std.format;
+import std.uni:toLower;
+import std.string;
 
 // void main() {
 extern (C) int main(int argc, const(char)** argv) {
     string msg = "Hello";
-    writeln(msg);
-
-
+    writeln(msg, '\t', (new MObject("Hello")).head("hello="));
     return 0;
 }
 
-static int x = 1234;
-
 unittest {
-    assert(x == 12351);
+    assert(false);
+}
+
+class MObject {
+    string value;
+
+    string tag() {
+        return typeof(this).stringof[1..$].toLower;
+    }
+
+    string val() {
+        return value;
+    }
+
+    string head(string prefix = "") {
+        return format("%s<%s:%s>", prefix, tag(), val());
+    }
+
+    this() {
+    }
+
+    this(string V) {
+        this.value = V;
+    }
+}
+
+class MPrimitive : MObject {
+}
+
+class MSym : MPrimitive {
+}
+
+class MStr : MPrimitive {
+}
+
+class MInt : MPrimitive {
+}
+
+class MContainer : MObject {
+}
+
+class MVector : MContainer {
+}
+
+class MStack : MContainer {
+}
+
+class MMap : MContainer {
+}
+
+class MActive : MObject {
+}
+
+class MVM : MActive {
+}
+
+class MCmd : MActive {
+}
+
+class MIO : MObject {
+}
+
+class MPath : MIO {
+}
+
+class MDir : MIO {
+}
+
+class MFile : MIO {
 }
