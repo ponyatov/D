99d03e0c10e3a5863cc9df563c235be2458c3faf
Dmitry Ponyatov <dponyatov@gmail.com>
2023-10-11 11:34:17 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- player/src/app.d
+++ player/src/app.d
@@ -1,7 +1,18 @@
-import std.stdio : writefln;
+import std.stdio : writefln, writeln;
 import std.range : enumerate;
 
-void main(string[] args) {
+import bindbc.sdl;
+import bindbc.sdl_image;
+
+int main(string[] args) {
     foreach (argc, argv; args.enumerate)
         writefln("arg[%d] = <%s>", argc, argv);
+    // dyn load
+    assert(loadSDL() == sdlSupport);
+    // assert(loadSDLImage() == sdlImageSupport);
+    //init
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        writeln("SDL_Init: ", SDL_GetError());
+    SDL_Quit();
+    return 0;
 }
