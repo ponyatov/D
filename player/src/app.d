d319828f0820d1c61c4bfebe30a71d1b362bbe2f
Dmitry Ponyatov <dponyatov@gmail.com>
2023-10-12 18:52:07 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- player/src/app.d
+++ player/src/app.d
@@ -6,6 +6,7 @@ import std.range : iota;
 import std.conv;
 
 import bindbc.sdl;
+import sdl.audio;
 
 // import bindbc.sdl_image;
 
@@ -14,6 +15,9 @@ auto mp3 = import("media/dwsample1.mp3");
 class AuDev {
     int id;
     string name;
+    int freq = Audio.freq;
+    SDL_AudioFormat format = Audio.format;
+    ubyte channels = Audio.channels;
     this(int id) {
         this.id = id;
         const char* _ = SDL_GetAudioDeviceName(id, false);
@@ -22,23 +26,88 @@ class AuDev {
         writeln(this);
     }
 
-    override string toString() const pure @safe {
-        return "<%s|%s:%s>".format(typeof(this).stringof, id, name);
+    string tag() const {
+        return typeof(this).stringof;
+    }
+
+    override string toString() const {
+        return "<%s|%s:%s> freq:%s ch:%s format:%s%s".format(tag,
+                id, name, freq, channels,
+                SDL_AUDIO_ISSIGNED(format) ? 's' : 'u',
+                SDL_AUDIO_BITSIZE(format));
     }
 }
 
-__gshared AuDev[] audev;
+enum Audio {
+    freq = 22_050,
+    format = AUDIO_S8,
+    channels = 1
+}
+
+class AuPlay : AuDev {
+    this(int id) {
+        super(id);
+    }
+
+    override string tag() const {
+        return typeof(this).stringof;
+    }
+
+    static callback(AuDev dev, ubyte[] stream, uint len) {
+        foreach (i; 0.iota(len)) {
+            stream[i] = cast(ubyte)(i % 0xFF);
+        }
+    }
+
+    void open(int freq = Audio.freq, SDL_AudioFormat format = Audio.format,
+            ubyte channels = Audio.channels) {
+    }
+
+    void play(string filename = "~/fx/media/dwsample1.mp3") {
+        SDL_AudioSpec desired, obtained;
+        SDL_zero(&desired);
+        SDL_zero(&obtained);
+        desired.freq = Audio.freq;
+        desired.format = Audio.format;
+        desired.channels = Audio.channels;
+        //
+        AuPlay dev = auplay[0];
+        dev.id = SDL_OpenAudioDevice(dev.name.toStringz, false,
+                &desired, &obtained, SDL_AUDIO_ALLOW_ANY_CHANGE);
+        if (dev.id <= 0) {
+            writefln("%s\t%s", dev.toString, SDL_GetError);
+            abort();
+        }
+        assert(desired.freq == obtained.freq);
+        dev.freq = obtained.freq;
+        assert(desired.format == obtained.format);
+        dev.format = obtained.format;
+        assert(desired.channels == obtained.channels);
+        dev.channels = obtained.channels;
+    }
+}
+
+class AuRec : AuDev {
+    this(int id) {
+        super(id);
+    }
+
+    override string tag() const {
+        return typeof(this).stringof;
+    }
+}
+
+__gshared AuPlay[] auplay;
+__gshared AuRec[] aurec;
 
 int main(string[] args) {
     foreach (argc, argv; args.enumerate)
         writefln("arg[%d] = <%s>", argc, argv);
     // dyn load: dub.json "bindbc-sdl": "dynamic"
-    static if (!bindbc.sdl.staticBinding) {
-        writeln("dynamic");
+    static if (!sdl.staticBinding) {
         assert(loadSDL() == sdlSupport);
         assert(loadSDLImage() == sdlImageSupport);
-    } else
-        writeln("static");
+    }
     // init
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         writeln("SDL_Init: ", SDL_GetError());
@@ -47,10 +116,14 @@ int main(string[] args) {
     scope (exit)
         SDL_Quit();
     // 
-    const auto auDevs = SDL_GetNumAudioDevices(false);
-    assert(auDevs);
-    foreach (i; auDevs.iota)
-        audev ~= new AuDev(i);
+    const auto auOuts = SDL_GetNumAudioDevices(false);
+    assert(auOuts);
+    foreach (i; auOuts.iota)
+        auplay ~= new AuPlay(i);
+    const auto auIns = SDL_GetNumAudioDevices(true);
+    assert(auIns);
+    foreach (i; auIns.iota)
+        aurec ~= new AuRec(i);
     //
     const winFlags = !SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN;
     const W = 240;
